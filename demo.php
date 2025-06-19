43f0893342a454cba4edd882004139b2944fb1f5
yanglong <2723659854@qq.com>
2025-06-19 22:27:15 +0800

    更新配置

--- demo.php
+++ demo.php
@@ -3,14 +3,14 @@ require_once 'vendor/autoload.php';
 
 /** 实例化客户端 elasticsearch链式操作演示 */
 $client = new \Xiaosongshu\Elasticsearch\ESClient(
-//    [
-//        'nodes' => ['127.0.0.1:9201'],
-//        'username' => 'elastic',
-//        'password' => '123456',
-//    ]
+    [
+        'nodes' => ['127.0.0.1:9200'],
+        'username' => '',
+        'password' => '',
+    ]
 );
 
 
-$result = $client->table('my_index')->max(['age'])->getAll();
+$result = $client->table('index','_doc')->getAll();
 
 var_dump($result);
